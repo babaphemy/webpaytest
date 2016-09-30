a6e73a461b9e3688f6286cd2f1819d052fc15304
babaphemy <babaphemy@gmail.com>
2016-09-30 09:26:11 +0100

    added query
    added query

--- incs/options.php
+++ incs/options.php
@@ -14,7 +14,15 @@
     $callbackpage = "http://localhost/webpaytest/tpay.php";
     $reference = dotref();
     $mackey = "D3D1D05AFE42AD50818167EAC73C109168A0F108F32645C8B59E897FA930DA44F9230910DAC9E20641823799A107A02068F7BC0F4CC41D2952E249552255710F";
-    
+    if(isset($_POST["pdtid"])){
+		$pd = $_POST["pdtid"];
+		$rf = $_POST["ref"];
+		$am = $_POST["amount"];
+		$mc = $_POST["mac"];
+		$response = query($pd,$am,$rf,$mc);
+		print_r($response);
+	}
+
     function dquery($amt,$ref){
         $pdt = $GLOBALS['pdtid'];
 		$thash = queryHash($ref);
@@ -57,6 +65,42 @@
 
 
     }
+
+function query($ppp, $am, $rff,$mac){		
+	//$query_url = 'https://stageserv.interswitchng.com/test_paydirect/api/v1/gettransaction.json';
+        $query_url = 'https://webpay.interswitchng.com/paydirect/api/v1/gettransaction.json';
+        $url 	= "$query_url?productid=$ppp&transactionreference=$rff&amount=$am";
+        //note the variables appended to the url as get values for these parameters
+		$tryhash = $ppp.$rff.$mac;
+        $dhash = hash('sha512', $tryhash);
+        $headers = array(
+            "GET /HTTP/1.1",
+            "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1) Gecko/2008070208 Firefox/3.0.1",
+            "Accept-Language: en-us,en;q=0.5",
+            "Keep-Alive: 300",
+            "Connection: keep-alive",
+            "Hash: $dhash " );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt( $ch, CURLOPT_POST, false );
+
+        $data = curl_exec($ch);
+        if (curl_errno($ch)) {
+            print "Error: " . curl_error($ch);
+        }
+        else {
+            $json = json_decode($data, TRUE);
+            curl_close($ch);
+            return $json;
+        }
+		
+		
+	}
 
 
     function dohash($amt1){
